f623eb0bbc012c7c47a1c252c1f76bfddd16752f
lemondedenemo <thomas.mallet@aviation-civile.gouv.fr>
2024-06-27 12:18:29 +0000

    debut sujet

---
+++ srcToto/create_data_list.py
@@ -0,0 +1,9 @@
+import yaml
+
+def create_data_list(source_file):
+
+    with open(source_file, 'r') as file:
+        catalogue = yaml.safe_load(file)
+    return catalogue
+
+urls = create_data_list("sources.yml")
